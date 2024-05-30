8cd6d24668a315209824bfaae17109910b9fcb10
jerai.garcia <jerai.garcia@inetum.com>
2024-05-30 12:47:20 +0200

    Añadir Peliculas

---
+++ Angular-Practica/src/app/models/peliculas.models.ts
@@ -0,0 +1,12 @@
+export interface PeliculasModel {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    categoria: string;
+    imagen: string;
+    fechaEstreno: Date;
+    duracion: number;
+    precio: number;
+    rating: number;
+
+}
