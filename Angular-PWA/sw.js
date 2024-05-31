92f2d2b00270f6e8c5f94d12d12ab5043abaf833
jerai.garcia <jerai.garcia@inetum.com>
2024-05-31 13:58:21 +0200

    Crear PWA

---
+++ Angular-PWA/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", ()=>{
+    console.log("Service worker installed")
+});
+
+self.addEventListener("activate", ()=>{
+    console.log("Service worker activated")
+});
+
+self.addEventListener("fetch", ()=>{
+    console.log("Service worker fetched")
+});
