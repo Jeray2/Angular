f95ed62b489002bea8708d7294a9d346b3fdc204
jerai.garcia <jerai.garcia@inetum.com>
2024-05-31 13:27:35 +0200

    Clase_Dos

--- Angular-Practica/src/app/components/hola-mundo/hola-mundo.component.ts
+++ Angular-Practica/src/app/components/hola-mundo/hola-mundo.component.ts
@@ -1,14 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hola-mundo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hola-mundo.component.html',
   styleUrl: './hola-mundo.component.css'
 })
 export class HolaMundoComponent {
-  titulo : String = "Hola Mundo desde el componente HolaMundoComponent";
-  color : String = "blue";
-  alumnos : String[] = ["Juan", "Pedro", "Maria", "Jose", "Javier"];
+  titulo : string = "Hola Mundo desde el componente HolaMundoComponent";
+  color : string = "blue";
+  alumnos : string[] = ["Juan", "Pedro", "Maria", "Jose", "Javier"];
+
+  mostrarDiv : boolean = false;
+
+  alternarDivs() {
+    this.mostrarDiv =!this.mostrarDiv;
+  }
+
+  AgregarAlumno(alumno : HTMLInputElement) {
+    this.alumnos.push(alumno.value);
+    alumno.value = "";
+  }
+
 }
